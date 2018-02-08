f875e35ef8ee304f8df0fba90430bba78463a066
Yongshuai Liu <lucasliu1118@gmail.com>
2018-02-07 22:57:57 -0700

    finish up firebase demo

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -1,3 +1,11 @@
 export const environment = {
-  production: true
+    production: true,
+    firebase: {
+        apiKey: "AIzaSyAz5zaDG1x3_m8MMFIGjzNP30kQyKlHR6E",
+        authDomain: "mycollection-ea352.firebaseapp.com",
+        databaseURL: "https://mycollection-ea352.firebaseio.com",
+        projectId: "mycollection-ea352",
+        storageBucket: "",
+        messagingSenderId: "29218564928"
+    }
 };
--- src/environments/environment.ts
+++ src/environments/environment.ts
@@ -4,5 +4,13 @@
 // The list of which env maps to which file can be found in `.angular-cli.json`.
 
 export const environment = {
-  production: false
+  production: false,
+  firebase: {
+      apiKey: "AIzaSyAz5zaDG1x3_m8MMFIGjzNP30kQyKlHR6E",
+      authDomain: "mycollection-ea352.firebaseapp.com",
+      databaseURL: "https://mycollection-ea352.firebaseio.com",
+      projectId: "mycollection-ea352",
+      storageBucket: "",
+      messagingSenderId: "29218564928"
+  }
 };
